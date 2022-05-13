9f777cdfca944dc00c681d7a641d8d7c7e2002bb
Jake Krummenacher <94008106+JakeKrummenacher@users.noreply.github.com>
2022-05-13 12:09:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var quotes = [];
+let quotes = [];
 quotes.push("Wow, to be born is to die. Problem solved!");
 quotes.push("Believe it or not I actually programmed this.");
 quotes.push("To dip is to die. Problem solved.");
@@ -7,10 +7,10 @@ quotes.push("We all die. You either kill yourself or get killed");
 quotes.push("Sports!");
 quotes.push("¡Deportes!");
 
-var quoteUsed = "Hey man";
+let quoteUsed = "Hey man";
 
 function getRandomQuote(){
-    var r = Math.floor(Math.random() * quotes.length);
+    let r = Math.floor(Math.random() * quotes.length);
     quoteUsed = quotes[r];
 }
 
